08b8779736c4131478ec462c050a1e09c3ebe5f1
Tae Kyu Kim <taekyukim02@gmail.com>
2021-08-02 01:52:15 -0700

    Update _includes and _layouts for MathJax second try

--- use_mathjax.sh
+++ use_mathjax.sh
@@ -2,5 +2,5 @@
 echo Creating _layouts and _includes in the current directory...
 mkdir -p _layouts
 mkdir -p _includes
-cp /usr/local/lib/ruby/gems/3.0.0/gems/minima-2.5.1/_layouts/default.html _layouts/defaults.html
+cp /usr/local/lib/ruby/gems/3.0.0/gems/minima-2.5.1/_layouts/default.html _layouts/default.html
 cp /usr/local/lib/ruby/gems/3.0.0/gems/minima-2.5.1/_includes/_mathjax_support.html _includes/_mathjax_support.html
