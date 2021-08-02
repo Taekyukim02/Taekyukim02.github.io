bfad4e880a94df66783707f5c8d6e538604fd5ec
Tae Kyu Kim <taekyukim02@gmail.com>
2021-08-02 01:48:08 -0700

    Upload _includes and _layout for MathJax

---
+++ use_mathjax.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo Creating _layouts and _includes in the current directory...
+mkdir -p _layouts
+mkdir -p _includes
+cp /usr/local/lib/ruby/gems/3.0.0/gems/minima-2.5.1/_layouts/default.html _layouts/defaults.html
+cp /usr/local/lib/ruby/gems/3.0.0/gems/minima-2.5.1/_includes/_mathjax_support.html _includes/_mathjax_support.html
